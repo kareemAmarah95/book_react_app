953955328546cc2155115b9ffe20d614b74599dc
kareemAmarah95 <kareemamarah2356@gmail.com>
2022-02-23 13:52:13 +0200

    simple book app

---
+++ src/Books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "by Amelia Hepworth and Tim Warnes",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
+    title: "Our Class is a Family",
+    author: "by Shannon Olsen and Sandie Sonke",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UY218_.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "by Brit Bennett, Shayna Small, et al.",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import "./index.css";
+// setup vars
+import {books} from "./Books";
+import Book  from "./Book";
+import {greeting} from "./testing/testing";
+function BookList() {
+  console.log("************ greeting : " ,greeting);
+  return (
+    <>
+      <section className="booklist">{books.map((book , idx)=>{
+        // console.log(book);
+        return (
+          <Book {...book} key={book.id}></Book>
+        );
+      })}</section>
+    </>
+  );
+}
+
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "h1",
+//     {},
+//     React.createElement("h1", {}, "hello world")
+//   );
+// };
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <BookList />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
